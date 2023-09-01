765a0e2a8ddc2b0486d9a294ef9858cec540a7c1
Andrew Martin <andrew.thaddeus@gmail.com>
2023-09-01 14:09:24 -0400

    Export Word32 vectors

---
+++ src/Vector/Word32.hs
@@ -0,0 +1,57 @@
+{-# language DataKinds #-}
+{-# language MagicHash #-}
+{-# language NumericUnderscores #-}
+{-# language BangPatterns #-}
+{-# language TypeApplications #-}
+{-# language TypeOperators #-}
+
+module Vector.Word32
+  ( -- Types
+    Vector(..)
+  , Vector#
+  , MutableVector(..)
+  , MutableVector#
+  , Bounded(..)
+  , Vector_(..)
+    -- * Primitives
+  , write#
+  , write
+  , read#
+  , index#
+  , index
+  , unlift
+  , substitute
+  , initialized
+  , unsafeCoerceLength
+  , expose
+  , expose#
+    -- * Ranges
+  , set
+  , setSlice
+    -- * Freeze
+  , unsafeShrinkFreeze
+  , unsafeFreeze
+  , freeze
+  , freezeSlice
+    -- * Copy
+  , thaw
+    -- * Composite
+  , map
+  , ifoldl'
+  , ifoldlSlice'
+  , replicate
+  , construct3
+  , construct4
+  , append
+  , clone
+  , cloneSlice
+    -- * Index
+  , index0
+  , index1
+  , index2
+  , index3
+  ) where
+
+import Prelude hiding (replicate,map,maximum,Bounded,all)
+
+import Vector.Std.Word32
